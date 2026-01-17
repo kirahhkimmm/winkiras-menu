4e35f19c0f74cd7ad8543fc647828e7a581b5ce0
winkira <kirahhkimmm@gmail.com>
2026-01-17 15:13:14 -0500

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,12 @@
+--[[
+  __        ___       _    _           _     
+\ \      / (_)_ __ | | _(_)_ __ __ _( )___     
+ \ \ /\ / /| | '_ \| |/ / | '__/ _` |// __|    made by kirahhkimmm on github
+  \ V  V / | | | | |   <| | | | (_| | \__ \    known as winkira
+   \_/\_/  |_|_| |_|_|\_\_|_|  \__,_| |___/    main ui consists of infinte yield
+         _ __ ___   ___ _ __  _   _            non of it is made by me just a combination of op scripts
+        | '_ ` _ \ / _ \ '_ \| | | |           recommend 8 level executor
+        | | | | | |  __/ | | | |_| |           --------------------------------------------------------
+        |_| |_| |_|\___|_| |_|\__,_|           enjoy
+]]
+
